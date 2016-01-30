ba4a5fa82c240602a4bfb46179fb3380daa54393
bravepower <bravepower@gmail.com>
2016-01-29 21:56:32 -0800

    Added grayboxing tool, lens flares, lasers, and set up folder structure.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Interactable : MonoBehaviour {
+
+    private const int LAYER_PLAYER = 8;
+
+    public bool IsActive { get; private set; }
+
+    public abstract void Activate();
+    public abstract void Deactivate();
+    public abstract void Use();
+
+    public virtual void Update()
+    {
+        if(IsActive)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Use();
+            }
+        }
+    }
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LAYER_PLAYER)
+        {
+            IsActive = true;
+            Activate();
+        }
+    }
+
+    public virtual void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == LAYER_PLAYER)
+        {
+            IsActive = false;
+            Deactivate();
+        }
+    }
+}

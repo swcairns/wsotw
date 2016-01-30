b072f33fd5c13c23b98d3f5e08b13223335841c7
Derek "The Coolest" Heck <dheck@03637a-dheck.corp.zynga.com>
2016-01-29 21:55:47 -0800

    Added "Grimm" space research lab assets

---
+++ Assets/Grim/Sci Fi Research Center/ReadMe.txt
@@ -0,0 +1,5 @@
+Thank you for your purchase of this asset.
+Please Do let me know if you would like anything added or changed in the next update and I will do so.
+
+Regards
+Grim
---
+++ Assets/Grim/Sci Fi Research Center/Scripts/Flickering light Blink.js
@@ -0,0 +1,19 @@
+﻿var flickering : boolean = true;
+ 
+function Start() {
+        flickerLight();
+}
+ 
+function Update () {
+ 
+}
+ 
+function flickerLight() {
+ 
+        while(flickering) {
+                yield WaitForSeconds(0.2);
+                gameObject.GetComponent.<Light>().intensity += 2;
+                yield WaitForSeconds(0.2);
+                gameObject.GetComponent.<Light>().intensity = 0;
+        }
+}

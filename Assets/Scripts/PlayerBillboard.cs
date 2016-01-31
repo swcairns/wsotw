a44674ea0fb0d610c81a331c3d743ae001860da2
Andrea Benavides <abenavides@OCCO-demo-PC.rws.ad.ea.com>
2016-01-31 02:19:35 -0800

    Music things and PlayerBillboard script.

---
+++ Assets/Scripts/PlayerBillboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBillboard : MonoBehaviour {
+
+    public Camera dasCamera;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(dasCamera.transform);
+	}
+}

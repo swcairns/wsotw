dfcd1418d826d964d4e16aa42e531054ed142926
Andrea Benavides <abenavides@OCCO-demo-PC.rws.ad.ea.com>
2016-01-31 22:06:39 -0800

    Glowy button things, music manager, and sfx galore.

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -4,34 +4,77 @@ using System.Collections;
 public class ButtonManager : MonoBehaviour {
 
     // Variables
-    int brightest = 10;
-    int dimmest = 1;
+    float brightest = 8f;
+    float dimmest = 0f;
 
-    //public Material glowMat;
-    //public struct HSBColor _targetColorHSB = stuff.
+    public Material mat;
+
+    float dur = 2f;
+    float speed = 10f;
+
+    private bool _isPulsing;
+
+    public bool IsPulsing
+    {
+        get { return _isPulsing; }
+        set
+        {
+            _isPulsing = value;
+            if (value) StartCoroutine("Pulsing");
+            else
+            {
+                StopCoroutine("Pulsing");
+                SetToDim();
+            }
+        }
+    }
 
     void Start()
     {
-
+        mat = transform.GetComponent<Renderer>().material;
+        this.IsPulsing = true;
     }
 
-    // Start a pulsing glow.
-
+    // Pulse the glow!
+    IEnumerator Pulsing()
+    {
+        Debug.Log("Pulsing!!");
+        Color emissionColor = mat.GetColor("_EmissionColor");
+        while(true)
+        {
+            float lerp = Mathf.PingPong(Time.time * speed, brightest - dimmest) + dimmest;
+            Debug.Log(lerp);
+            //Color emissionColor = new Color(mat.color.r, mat.color.g, mat.color.b, mat.color.a) * Mathf.LinearToGammaSpace(lerp);
+            mat.SetColor("_EmissionColor", emissionColor * Mathf.LinearToGammaSpace(lerp));
+            yield return null;
+        }
+    }
 
     // However bright it is, dim the brightness down to the dimmest value.
-    void DimDown()
+    void SetToDim ()
     {
-        StartCoroutine("Dimmer");
+        Color emissionColor = mat.GetColor("_EmissionColor");
+        mat.SetColor("_EmissionColor", emissionColor * Mathf.LinearToGammaSpace(dimmest));
     }
 
-    IEnumerator Dimmer()
+    /*
+    IEnumerator FadeInTrack(AudioSource track, float vol = 1)
     {
-        //GetComponent<Renderer>().material.SetColor("_EmissionColor", HSBColor.ToColor(HSBColor.Lerp(HSBColor.FromColor(GetComponent<Renderer>().material.color), _targetColorHSB, Time.deltaTime / timeLeft)));
-        yield break;
+        //Debug.Log("track volume: " + track.volume);
+        float min = track.volume;
+        float timeToChange = 0;
+        while (track.volume < vol)
+        {
+            timeToChange += Time.deltaTime * fadeTime;
+            track.volume = Mathf.Lerp(min, vol, timeToChange);
+            // Debug.Log("track volume after lerp: " + track.volume);
+            yield return null;
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
+    */
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,6 +4,10 @@ using System.Collections;
 
 public class MusicManager : MonoBehaviour {
 
+    // DAY TRANSITION MUSIC
+    //public AudioSource tran_evening;
+    //public AudioSource tran_morning;
+
     // AMBIENT
     public AudioSource amb_engineNoise;
     public AudioSource amb_telemetry;
@@ -37,22 +41,58 @@ public class MusicManager : MonoBehaviour {
     bool shipIsFirst = true;
     bool narrativeIsFirst = true;
 
-    int fadeTime = 1;
+    float fadeTime = .5f;
+    float enviroVol = .5f;
 
+    // Listen for shit.
+    void OnEnable()
+    {
+        EventManager.StartListening("new_day", HandleDayStart);
+        //EventManager.StartListening("end_day", HandleDayStart);
+
+        // Rituals
+        EventManager.StartListening("ritual_personal", HandleRitualStart_Personal);
+        EventManager.StartListening("ritual_ship", HandleRitualStart_Ship);
+        EventManager.StartListening("ritual_narrative", HandleRitualStart_Narrative);
+
+        //EventManager.StartListening("ritual_strike_personal", HandleRitualStart_Narrative);
+        //EventManager.StartListening("ritual_strike_ship", HandleRitualStart_Narrative);
+        //EventManager.StartListening("ritual_strike_narrative", HandleRitualStart_Narrative);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("new_day", HandleDayStart);
+        //EventManager.StopListening("end_day", HandleDayStart);
+
+        // Rituals
+        EventManager.StopListening("ritual_personal", HandleRitualStart_Personal);
+        EventManager.StopListening("ritual_ship", HandleRitualStart_Ship);
+        EventManager.StopListening("ritual_narrative", HandleRitualStart_Narrative);
+
+        //EventManager.StopListening("ritual_strike_personal", HandleRitualStart_Narrative);
+        //EventManager.StopListening("ritual_strike_ship", HandleRitualStart_Narrative);
+        //EventManager.StopListening("ritual_strike_narrative", HandleRitualStart_Narrative);
+    }
 
     // Use this for initialization
     void Start() {
 
         // Create lists of all the tracks.
-        foreach (AudioSource track in transform.GetComponents<AudioSource>())
+        AudioSource[] tempAudioList = transform.GetComponents<AudioSource>();
+        foreach (AudioSource track in tempAudioList)
         //for (int x = 2; x < transform.GetComponents<AudioSource>().Length; x++)
         {
+            //Debug.Log("track is: " + track);
+            //Debug.Log("clip is: " + track.clip);
             audioList.Add(track);
             track.loop = true;
             track.playOnAwake = false;
             track.volume = 0;
         }
-        // Remove the ambient sound tracks.
+        // Remove the ambient and transition sound tracks.
+        //audioList.Remove(audioList[3]);
+        //audioList.Remove(audioList[2]);
         audioList.Remove(audioList[1]);
         audioList.Remove(audioList[0]);
 
@@ -69,17 +109,24 @@ public class MusicManager : MonoBehaviour {
     void ResetTracks()
     {
         // Set the "current" track for each ritual type.
-        cur_personal = neut_narrative_1st;
+        //cur_personal = neg_personal_1st;
+        cur_personal = neut_personal_1st;
         cur_ship = neut_ship_1st;
         cur_narrative = neut_narrative_1st;
 
+        //Debug.Log("current personal: " + cur_personal);
     }
 
     void HandleDayStart()
     {
+        Debug.Log("Start Day!");
+        // Play the ambient sounds.
+        amb_engineNoise.Play();
+        amb_telemetry.Play();
+
         // Fade in the ambient sounds.
-        StartCoroutine("FadeInTrack", amb_engineNoise);
-        StartCoroutine("FadeInTrack", amb_telemetry);
+        StartCoroutine(FadeInTrack(amb_engineNoise, enviroVol));
+        StartCoroutine(FadeInTrack(amb_telemetry, enviroVol));
     }
 
     void HandleRitualStart()
@@ -99,37 +146,59 @@ public class MusicManager : MonoBehaviour {
         // Manage the Personal track.
         if (cur_personal.isPlaying && personalIsFirst)
         {
-            if (cur_personal.timeSamples >= cur_personal.GetComponent<AudioClip>().samples)
+            //Debug.Log("cur_personal timeSamples = " + cur_personal.timeSamples + ", cur_personal clip samples = " + cur_personal.clip.samples);
+            if (cur_personal.timeSamples >= cur_personal.clip.samples) //cur_personal.GetComponent<AudioClip>().samples)
             {
+                Debug.Log("Swapping personal track!");
                 cur_personal.Stop();
-                cur_personal = (cur_personal == neut_personal_1st) ? neut_personal_2nd : neg_personal_2nd;
+                Debug.Log("old track: " + cur_personal.clip);
+                float tempVol = cur_personal.volume;
+                cur_personal = (cur_personal.clip == neut_personal_1st.clip) ? neut_personal_2nd : neg_personal_2nd;
+                Debug.Log("new track: " + cur_personal.clip);
+                cur_personal.volume = tempVol;
                 cur_personal.Play();
+                personalIsFirst = false;
             }
-            personalIsFirst = false;
         }
         // Manage the Ship track.
         if (cur_ship.isPlaying && shipIsFirst)
         {
-            if (cur_ship.timeSamples >= cur_ship.GetComponent<AudioClip>().samples)
+            if (cur_ship.timeSamples >= cur_ship.clip.samples) //cur_ship.GetComponent<AudioClip>().samples)
             {
                 cur_ship.Stop();
                 cur_ship = (cur_ship == neut_ship_1st) ? neut_ship_2nd : neg_ship_2nd;
                 cur_ship.Play();
+                shipIsFirst = false;
             }
-            shipIsFirst = false;
         }
         // Manage the Narrative track.
         if (cur_narrative.isPlaying && narrativeIsFirst)
         {
-            if (cur_narrative.timeSamples >= cur_narrative.GetComponent<AudioClip>().samples)
+            if (cur_narrative.timeSamples >= cur_narrative.clip.samples) //cur_narrative.GetComponent<AudioClip>().samples)
             {
                 cur_narrative.Stop();
                 cur_narrative = (cur_narrative == neut_narrative_1st) ? neut_narrative_2nd : neg_narrative_2nd;
                 cur_narrative.Play();
+                narrativeIsFirst = false;
             }
-            narrativeIsFirst = false;
         }
 
+    }
+
+    void HandleRitualStart_Personal()
+    {
+        Debug.Log("Started Personal Ritual!");
+        StartCoroutine(FadeInTrack(cur_personal));
+    }
+
+    void HandleRitualStart_Ship()
+    {
+        StartCoroutine("FadeInTrack", cur_ship);
+    }
+
+    void HandleRitualStart_Narrative()
+    {
+        StartCoroutine("FadeInTrack", cur_narrative);
     }
 
     void HandleRitualStrike()
@@ -153,18 +222,20 @@ public class MusicManager : MonoBehaviour {
     // foreach (AudioSource track in audioTrack) fade to black
 	
 	// Fade between two tracks.
-    void FadeTwoTracks (AudioSource cur, AudioSource target)
+    void FadeTwoTracks (AudioSource cur, AudioSource target, float vol)
     {
         StartCoroutine("FadeOutTrack", cur);
-        StartCoroutine("FadeInTrack", target);
+        StartCoroutine(FadeInTrack(target, vol));
     }
 
     IEnumerator FadeOutTrack(AudioSource track)
     {
         float max = track.volume;
+        float timeToChange = 0;
         while (track.volume > 0)
         {
-            track.volume = Mathf.Lerp(max, 0, Time.time * fadeTime);
+            timeToChange += Time.deltaTime * fadeTime;
+            track.volume = Mathf.Lerp(max, 0, timeToChange);
             yield break;
         }
     }
@@ -172,21 +243,27 @@ public class MusicManager : MonoBehaviour {
     IEnumerator FadeOutAndEndTrack(AudioSource track)
     {
         float max = track.volume;
+        float timeToChange = 0;
         while (track.volume > 0)
         {
-            track.volume = Mathf.Lerp(max, 0, Time.time * fadeTime);
-            yield break;
+            timeToChange += Time.deltaTime * fadeTime;
+            track.volume = Mathf.Lerp(max, 0, timeToChange);
+            yield return null;
         }
         track.Stop();
     }
 
-    IEnumerator FadeInTrack (AudioSource track)
+    IEnumerator FadeInTrack (AudioSource track, float vol = 1)
     {
+        //Debug.Log("track volume: " + track.volume);
         float min = track.volume;
-        while (track.volume < 1)
+        float timeToChange = 0;
+        while (track.volume < vol)
         {
-            track.volume = Mathf.Lerp(min, 1, Time.time * fadeTime);
-            yield break;
+            timeToChange += Time.deltaTime * fadeTime;
+            track.volume = Mathf.Lerp(min, vol, timeToChange);
+            // Debug.Log("track volume after lerp: " + track.volume);
+            yield return null;
         }
     }
 }
